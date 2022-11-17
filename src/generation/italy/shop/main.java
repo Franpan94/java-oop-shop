26515794cdd4a042a79c363a1bfa3baf3de2953e
Franpan94 <francescopanico94@libero.it>
2022-11-17 17:12:13 +0100

    last commit

--- src/generation/italy/shop/main.java
+++ src/generation/italy/shop/main.java
@@ -1,5 +1,22 @@
 package generation.italy.shop;
 
-public class main {
+import java.util.Scanner;
 
+public class main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Inserisci nome prodotto: ");
+		String name = sc.nextLine();
+		
+		System.out.print("Inserisci descrizione prodotto: ");
+		String description = sc.nextLine();
+		
+		System.out.print("Inserisci prezzo prodotto: ");
+		float price = sc.nextFloat();
+		
+		product prod = new product(name, description, price);
+		System.out.println(prod);
+		
+	}
 }
--- src/generation/italy/shop/product.java
+++ src/generation/italy/shop/product.java
@@ -1,5 +1,41 @@
 package generation.italy.shop;
 
-public class product {
+import java.util.Random;
 
+public class product {
+	 int code;
+	 float price;
+	 String description;
+	 String name;
+	 float iva;
+	 
+     public product( String name, String description, float price) {
+    	 this.price = price;
+    	 this.description = description;
+    	 this.name= name;
+    	 
+    	 Random rnd = new Random();
+    	 code = rnd.nextInt(10000, 100000);
+    	 
+    	 iva = 20;
+    	 
+	}
+     public float price() {
+    	 return price;
+     }
+     
+     public float priceIva() {
+    	 return price + (price * iva)/(float)100;
+     }
+     
+     public String toString() {
+    	 
+ 		return  "---------------------------------------" +
+ 				"\nNome prodotto: " + name + "\nDescrizione prodotto: " + description + 
+ 				"\nCodice prodotto: " + code + "\nPrezzo prodotto = " + price() + 
+ 				"\nPrezzo incrementato dall'iva = " + priceIva();
+
+ 	}
+    	 
+     
 }
